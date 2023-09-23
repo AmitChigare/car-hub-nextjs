ead08593f629c0443f06ff8731013045f78e88e3
AmitChigare <amit.gokakfalls17@gmail.com>
2023-09-23 10:46:29 +0530

    Implmented CarCard and Card Details page functionality and design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { CarCard } from "@/components";
 import CustomFilter from "@/components/CustomFilter";
 import Hero from "@/components/Hero";
 import SearchBar from "@/components/SearchBar";
+import { fetchCars } from "@/utlis";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -21,6 +26,25 @@ export default function Home() {
             {/* <CustomFilter title="year" options={yearsOfProduction} /> */}
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+            {/* <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            /> */}
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
